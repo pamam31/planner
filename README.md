9d0185194d27fa8eb2387af421cdb057d08d49fe
pamam31 <pamcg31@gmail.com>
2024-02-16 22:12:38 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# daily-planner
+## Description
+This is a daily planner app that allows the user to track their daily activities.
+
+## Installation
+CLick [here](url) for the deployed app
+
+## Usage
+![alt text](daily-scheduler.png)
+This web app allows the user to plan their day. Simply add an activity in a green time block. The green time block represents free/future time. Clicking on the save icon allows users to save activities or whatever they choose to input in the tim block.
+The red time block represents present time, while the grey time block represents past time. 
+
+## Credits
+N/A
+
+## License
+MIT license. Please refer to the license in the repo.
---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    // Display current day
+    $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+  
+    // Get the current hour
+    const currentHour = dayjs().hour();
+  
+    // Create time blocks
+    const container = $(".container");
+  
+    for (let hour = 00; hour <= 23; hour++) {
+      const timeBlock = $("<div>").addClass("row time-block");
+      const hourBlock = $("<div>").addClass("col-1 hour").text(`${hour}:00`);
+      const textArea = $("<textarea>").addClass("col-10 description").attr("data-hour", hour);
+      const saveBtn = $("<button>").addClass("col-1 saveBtn").html('<i class="fas fa-save"></i>');
+  
+      // Append elements to time block
+      timeBlock.append(hourBlock, textArea, saveBtn);
+      container.append(timeBlock);
+  
+      // Color code time blocks based on past, present, or future
+      if (hour < currentHour) {
+        timeBlock.addClass("past");
+      } else if (hour === currentHour) {
+        timeBlock.addClass("present");
+      } else {
+        timeBlock.addClass("future");
+      }
+  
+      // Load events from local storage
+      const storedEvent = localStorage.getItem(`event-hour-${hour}`);
+      if (storedEvent) {
+        textArea.val(storedEvent);
+      }
+    }
+  
+    // Event listener for save buttons
+    $(".saveBtn").on("click", function () {
+      const hour = $(this).siblings(".description").attr("data-hour");
+      const eventText = $(this).siblings(".description").val();
+  
+      // Save to local storage
+      localStorage.setItem(`event-hour-${hour}`, eventText);
+    });
+  });
+  
